b7f980b295ec2d70082c40e0317b7fa7d143b85c
e <18790336164@163.com>
2021-01-31 18:20:16 +0800

    添加vue文章

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,120 +1,125 @@
 module.exports = {
-  "title": "前端搬运工",
-  "description": "我们不生成代码,我们只是代码搬运工.",
-  "dest": "public",
-  "base":"/Interview/",
-  "head": [
+  title: "前端搬运工",
+  description: "我们不生成代码,我们只是代码搬运工.",
+  dest: "public",
+  base: "/Interview/",
+  head: [
     [
       "link",
       {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
+        rel: "icon",
+        href: "/favicon.ico",
+      },
     ],
     [
       "meta",
       {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
   ],
-  "theme": "reco",
-  "themeConfig": {
-    "nav": [
+  theme: "reco",
+  themeConfig: {
+    nav: [
       {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
+        text: "首页",
+        link: "/",
+        icon: "reco-home",
       },
       {
-        "text": "TimeLine",
-        "link": "/timeline/",
-        "icon": "reco-date"
+        text: "归档",
+        link: "/timeline/",
+        icon: "reco-date",
       },
       {
-        "text": "Docs",
-        "icon": "reco-message",
-        "items": [
+        text: "更多",
+        icon: "reco-message",
+        items: [
           {
-            "text": "vuepress-reco",
-            "link": "/docs/theme-reco/"
-          }
-        ]
+            text: "每日一题",
+            link: "/docs/questions/",
+          },
+        ],
       },
       {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
+        text: "联系我",
+        icon: "reco-message",
+        items: [
           {
-            "text": "GitHub",
-            "link": "https://github.com/recoluan",
-            "icon": "reco-github"
-          }
-        ]
-      }
-    ],
-    "sidebar": {
-      "/docs/theme-reco/": [
-        "",
-        "theme",
-        "plugin",
-        "api"
-      ]
-    },
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "Category"
+            text: "GitHub",
+            link: "https://github.com/dakang824",
+            icon: "reco-github",
+          },
+        ],
       },
-      "tag": {
-        "location": 3,
-        "text": "Tag"
-      }
-    },
-    "friendLink": [
-      {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
-      },
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      }
     ],
-    "logo": "/logo.png",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "lastUpdated": "Last Updated",
-    "author": "yukang",
-    "authorAvatar": "https://dakang824.github.io/imgs/folder_a/132.jpg",
-    "record": "xxxx",
-    "startYear": "2017"
+    sidebar: {
+      "/docs/questions/": ["theme", "plugin", "api"],
+    },
+    type: "blog",
+    blogConfig: {
+      category: {
+        location: 2,
+        text: "前端",
+      },
+      tag: {
+        location: 3,
+        text: "标签",
+      },
+    },
+    friendLink: [
+      {
+        title: "vuepress-theme-reco",
+        desc: "A simple and beautiful vuepress Blog & Doc theme.",
+        avatar:
+          "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        link: "https://vuepress-theme-reco.recoluan.com",
+      },
+    ],
+    logo: "/logo.png",
+    search: true,
+    mode: "dark",
+    subSidebar: "auto",
+    searchMaxSuggestions: 10,
+    lastUpdated: "Last Updated",
+    author: "yukang",
+    authorAvatar: "https://dakang824.github.io/imgs/folder_a/132.jpg",
+    record: "xxxx",
+    startYear: "2017",
   },
-  "plugins": [
+  plugins: [
     [
       "@vuepress-reco/vuepress-plugin-kan-ban-niang",
       {
-        theme: ['haruto', 'blackCat', 'whiteCat', 'haru1', 'haru2', 'koharu', 'izumi', 'shizuku', 'wanko', 'miku', 'z16'],
+        theme: [
+          "haruto",
+          "blackCat",
+          "whiteCat",
+          "haru1",
+          "haru2",
+          "koharu",
+          "izumi",
+          "shizuku",
+          "wanko",
+          "miku",
+          "z16",
+        ],
         clean: false,
+        modelStyle: { left: "20px", bottom: "-20px", opacity: "0.9" },
+        btnStyle: { left: "155px", bottom: "40px" },
+        messageStyle: { left: "68px", bottom: "190px" },
         messages: {
-          welcome: '我是lookroot欢迎你的关注 ',
-          home: '心里的花，我想要带你回家。',
-          theme: '好吧，希望你能喜欢我的其他小伙伴。',
-          close: '再见哦'
-        }
-      }
+          welcome: "我是lookroot欢迎你的关注 ",
+          home: "心里的花，我想要带你回家。",
+          theme: "好吧，希望你能喜欢我的其他小伙伴。",
+          close: "再见哦",
+        },
+      },
     ],
-    [
-      'cursor-effects',
-    ]
+    ["cursor-effects"],
   ],
-  "markdown": {
-    "lineNumbers": true
-  }
-}
+  markdown: {
+    lineNumbers: true,
+  },
+};
