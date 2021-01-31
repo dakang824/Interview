dd3ff11c69d7d7656d99e27a0c8250f9e22b1dad
e <18790336164@163.com>
2021-01-31 13:54:06 +0800

    首次提交

---
+++ .vuepress/config.js
@@ -0,0 +1,119 @@
+module.exports = {
+  "title": "前端搬运工",
+  "description": "我们不生成代码,我们只是代码搬运工.",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Docs",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "vuepress-reco",
+            "link": "/docs/theme-reco/"
+          }
+        ]
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "yukang",
+    "authorAvatar": "https://dakang824.github.io/imgs/folder_a/132.jpg",
+    "record": "xxxx",
+    "startYear": "2017"
+  },
+  "plugins": [
+    [
+      "@vuepress-reco/vuepress-plugin-kan-ban-niang",
+      {
+        theme: ['haruto', 'blackCat', 'whiteCat', 'haru1', 'haru2', 'koharu', 'izumi', 'shizuku', 'wanko', 'miku', 'z16'],
+        clean: false,
+        messages: {
+          welcome: '我是lookroot欢迎你的关注 ',
+          home: '心里的花，我想要带你回家。',
+          theme: '好吧，希望你能喜欢我的其他小伙伴。',
+          close: '再见哦'
+        }
+      }
+    ],
+    [
+      'cursor-effects',
+    ]
+  ],
+  "markdown": {
+    "lineNumbers": true
+  }
+}
